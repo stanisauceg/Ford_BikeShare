94ca7fc01c2d6fc89e4963c577ddac45f1172f73
stanisauceg <mishahutson@gmail.com>
2019-04-08 16:15:49 -0400

    mucking around with maps

--- src/Ford_BikeShare_DeeperDive.R
+++ src/Ford_BikeShare_DeeperDive.R
@@ -48,6 +48,116 @@ g + geom_point(aes(x = start_station_longitude, y = start_station_latitude),
   labs(title = "SF hourly bike traffic", x = "Longitude", y = "Latitude", 
        size = "departures \nand arrivals \nper hour")
 
+library(raster)
+sfn_dem <- raster("data/RGIS1_Data/sanfrancisconorth.dem")
+sfn_dem %>% crs()
+summary(sfn_dem, maxsamp = ncell(sfn_dem))
+plot(sfn_dem)
+sfn_dem_df <- as.data.frame(sfn_dem, xy = TRUE)
+str(sfn_dem_df)
+pryr::object_size(sfn_dem_df)
+
+ggplot() +
+  geom_raster(data = sfn_dem_df , aes(x = x, y = y, fill = sanfrancisconorth)) +
+  scale_fill_viridis_c(option = "B") +
+  coord_quickmap()
+
+crs(sfn_dem)
+
+library(sf)
+sf_shp <- st_read("http://spatial.lib.berkeley.edu/public/ark28722-s7d02x/data.zip")
+sf_shp <- st_read("data/BayArea/bayarea_general.shp")
+bikeways <- st_read("data/SF_bikeways/bikeways.shp")
+st_geometry_type(sf_shp)
+st_crs(sf_shp)
+st_bbox(sf_shp)
+st_bbox(bikeways)
+sf_shp
+plot(sf_shp)
+
+bikeways_simple <- st_simplify(bikeways)
+
+plot(sfn_dem)
+plot(sf_shp_crs$geometry, add=TRUE)
+plot(bikeways_crs$geometry, col = "blue", add = TRUE)
+
+library(rasterVis)
+gplot(sfn_dem) +
+  geom_raster(aes(fill = value)) +
+  geom_sf(data = sf_shp, color = "black", fill = "NA") +
+  # geom_sf(data = bikeways, color = "blue") +
+  # coord_sf(xlim = c(-122.6, -122.3), ylim = c(37.6, 37.9), crs = the_crs) +
+  theme_light()
+
+crs(sfn_dem)
+st_crs(bikeways_crs)
+st_crs(sf_shp_crs)
+the_crs <- crs(sfn_dem, asText = TRUE)
+
+crs(sfn_dem)
+sfn_dem_crs <- projectRaster(sfn_dem, crs = crs(bikeways), method = "ngb")
+sfn_dem_crs_df <- as.data.frame(sfn_dem_crs, xy = TRUE)
+bikeways_crs <- st_transform(bikeways, crs = crs(sf_shp))
+sf_shp_crs <- st_transform(sf_shp, crs = crs(bikeways))
+
+#library(rasterVis)
+library(viridis)
+# library(rvest)
+library(tmap)
+
+tm_shape(sfn_dem) + tm_raster() + 
+  tm_shape(sf_shp_crs) + tm_borders() + 
+  tm_shape(bikeways_crs) + tm_lines(col = "blue", alpha = 0.3)
+
+roi <- st_bbox(c(xmin = -122.5, xmax = -122.37,
+          ymin = 37.75, ymax = 37.83),
+        crs = 4326) %>%
+  st_as_sfc() %>% 
+  st_transform(crs = the_crs) 
+
+tm_shape(sfn_dem, bbox = roi) + tm_raster() + 
+  tm_shape(sf_shp_crs) + tm_borders() + 
+  tm_shape(bikeways_crs) + tm_lines(col = "blue", alpha = 0.3)
+
+sample_day_sf <- st_as_sf(sample_day, coords = c("start_station_longitude", "start_station_latitude"), crs = crs(bikeways))
+
+station_launch <- data_clean %>% 
+  group_by(start_station_name) %>% 
+  top_n(n= -1, wt = start_time) %>% 
+  ungroup() %>% 
+  dplyr::select(c(start_station_id, start_time)) %>%
+  arrange(start_station_id)
+
+tm_shape(sfn_dem, bbox = roi) + tm_raster() + 
+  tm_shape(sf_shp_crs) + tm_borders() + 
+  tm_shape(bikeways_crs) + tm_lines(col = "blue") +
+  tm_shape(sample_day_sf) + tm_dots()
+
+sf_shp %>% bind_cols(start_time = 1)
+
+ggplot(sample_day_sf) +
+  # geom_raster(data = sfn_dem_crs_df, aes(x = x, y = y, fill = sanfrancisconorth)) +
+  geom_sf(data = sf_shp_crs, color = "black", fill = "NA") +
+  geom_sf(data = bikeways, color = "blue") +
+  geom_segment(data = sample_day, aes(x = start_station_longitude, y = start_station_latitude,
+                  xend = end_station_longitude, yend = end_station_latitude),
+              alpha = 0.1, show.legend = FALSE) +
+  geom_point(data = sample_day, aes(x = start_station_longitude, y = start_station_latitude),
+                           color = "blue", alpha = 0.2, stat = "sum") +
+  geom_point(data = sample_day, aes(x = end_station_longitude, y = end_station_latitude),
+            color = "red", alpha = 0.15, stat = "sum") +
+  scale_size(range = c(.1, 5)) +
+  xlim(c(-122.48, -122.37)) + 
+  ylim(c(37.74, 37.81)) +
+  # facet_wrap(~ hour(start_time)) +
+  theme_light() +
+  # geom_sf(data = sample_day_sf) +
+  labs(title = "SF hourly bike traffic", x = "Longitude", y = "Latitude", 
+       size = "Departures \nand arrivals \nper hour")
+
+
+plot(st_geometry(sample_day_sf))
+
 rm(figure_data, sample_day, sample_day_highlights)
 
 #### *** Question 2: How does elevation influence rides? #####################################
